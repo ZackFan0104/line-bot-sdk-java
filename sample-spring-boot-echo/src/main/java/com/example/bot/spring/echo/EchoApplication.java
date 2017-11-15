f125ee22946225f7712f59fb5c093e6e17dcbdfd
Zack <fanchekai@gmail.com>
2017-11-15 17:16:39 +0800

    getid

--- sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
+++ sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
@@ -36,8 +36,7 @@ public class EchoApplication {
     @EventMapping
     public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
         System.out.println("event: " + event);
-        System.out.println("----- TEXT -----: " + event.getMessage().getText())
-        return new TextMessage(event.getMessage().getText());
+        return new TextMessage(event.getMessage().getId());
     }
 
     @EventMapping
